4acb6ad6629dfe43e0446ef1af0e525754d224ac
Diego Beghin <diego.beghin@gmail.com>
2018-06-02 22:33:14 +0200

    debug

--- MuTau_copytree2017.cc
+++ MuTau_copytree2017.cc
@@ -64,14 +64,13 @@ void IIHEAnalysis::Loop(string phase, string type_of_data, string out_name, stri
       if (iEntry < 0) break;
       if (jEntry % 1000 == 0) fprintf(stdout, "\r  Processed events: %8d of %8d ", jEntry, nEntries);
       
-      cout << endl << "1st" << endl;
       nb = fChain->GetEntry(jEntry);
       nbytes += nb;
 
 
       //Is one of the triggers fired?
       bool PassMuonTrigger = false;
-      if (trig_HLT_IsoMu27_accept /*|| trig_HLT_IsoTkMu24_accept*/ || trig_HLT_Mu50_accept /*|| trig_HLT_TkMu50_accept*/) PassMuonTrigger = true;
+      if (trig_HLT_IsoMu27_accept) PassMuonTrigger = true;
       if (!PassMuonTrigger) continue;
 
 
@@ -87,31 +86,6 @@ void IIHEAnalysis::Loop(string phase, string type_of_data, string out_name, stri
       //hCounter2->Fill(1, event_weight);
 
 
-      //start muon counting loop
-      int Nmu = 0;
-      /*for (unsigned int iMu = 0; iMu < mu_gt_pt->size(); ++iMu) {
-	if(mu_isPFMuon->at(iMu) && mu_gt_pt->at(iMu) > 20 && fabs(mu_gt_eta->at(iMu)) < 2.4 && fabs(mu_gt_dxy_firstPVtx->at(iMu)) < 0.045 && fabs(mu_gt_dz_firstPVtx->at(iMu)) < 0.2 && mu_pfIsoDbCorrected04->at(iMu) < 0.1 && mu_isMediumMuon->at(iMu)) ++Nmu;
-	if (Nmu > 2) break;
-      }
-      if (Nmu > 2) continue; //3rd muon veto
-      //There will be a veto on the presence of a 2nd muon in mutau case
-
-      //electron veto
-      bool electron = false;
-      for (unsigned int iEle = 0; iEle < gsf_pt->size(); ++iEle) {
-	if (gsf_VIDLoose->at(iEle) && gsf_pt->at(iEle) > 20 && fabs(gsf_eta->at(iEle)) < 2.5 && fabs(gsf_dxy_firstPVtx->at(iEle)) < 0.045 && fabs(gsf_dz_firstPVtx->at(iEle)) < 0.2 && gsf_passConversionVeto->at(iEle) && gsf_nLostInnerHits->at(iEle) <= 1 && gsf_relIso->at(iEle) < 0.1) electron = true;
-	if (electron) break;
-       }
-       if (electron) continue;*/
-
-      //bjet veto (medium WP for the bjet)
-      /*bool bjet = false;
-      for (unsigned int iJet = 0; iJet < jet_pt->size(); ++iJet) {
-	if (jet_CSVv2->at(iJet) > 0.800 && jet_pt->at(iJet) > 20 && fabs(jet_eta->at(iJet)) < 2.4) bjet = true;
-	if (bjet) break;
-      }
-      if (bjet) continue;*/
-
       
       //start loop over reconstructed muons
       bool tightmuon = false; //for the tag-and-probe and the High Mass LFV analysis, we need a tight muon
@@ -119,40 +93,27 @@ void IIHEAnalysis::Loop(string phase, string type_of_data, string out_name, stri
       bool tauormu = false; //for the tag-and-probe, we need mumu and mutau events
       TLorentzVector tauormu_p4;
       for (unsigned int iMu = 0; iMu < mu_gt_pt->size(); ++iMu) {
-	cout << "a" << iMu << endl;
 	//is there a tight muon?
 	if (mu_gt_pt->at(iMu) < 27.0) continue;
-	cout << "b" << iMu << endl;
 	if (fabs(mu_gt_eta->at(iMu)) > 2.4) continue;
-	cout << "c" << iMu << endl;
-	if (!(/*mu_isPFMuon->at(iMu) || mu_isHighPtMuon->at(iMu) ||*/ mu_isMediumMuon->at(iMu))) continue; //medium ID or HighPt
-	cout << "d" << iMu << endl;
+	if (!mu_isPFMuon->at(iMu)) continue;
+	if (!mu_isMediumMuon->at(iMu)) continue; //medium ID
 	if (fabs(mu_gt_dxy_firstPVtx->at(iMu)) > 0.045) continue;
-	cout << "e" << iMu << endl;
 	if (fabs(mu_gt_dz_firstPVtx->at(iMu)) > 0.2) continue;
-	if (mu_pfIsoDbCorrected04->at(iMu) > 0.3) continue;
 	
 	tightmu_p4.SetPtEtaPhiM(mu_gt_pt->at(iMu), mu_gt_eta->at(iMu), mu_gt_phi->at(iMu), mu_mass);
 	tightmuon = true;
 
 	//is there a 2nd, acceptable muon?
 	//careful not to consider the same muon as in the first loop
-	for (unsigned int iMu2 = 0; iMu2 < mu_gt_pt->size(); ++iMu2) {
-	  if (iMu2 == iMu) continue; 
+	for (unsigned int iMu2 = 0; iMu2 < iMu; ++iMu2) {
 	  if (mu_gt_pt->at(iMu2) < 20.0) continue;
 	  if (fabs(mu_gt_eta->at(iMu2)) > 2.4) continue;
-	  if (mu_pfIsoDbCorrected04->at(iMu2) > 0.3) continue;
-
+	  if (!mu_isPFMuon->at(iMu2)) continue;
+	  if (!mu_isMediumMuon->at(iMu2)) continue; //medium ID
+	  if (fabs(mu_gt_dxy_firstPVtx->at(iMu2)) > 0.045) continue;
+	  if (fabs(mu_gt_dz_firstPVtx->at(iMu2)) > 0.2) continue;
 	  tauormu = true;
-
-	  /*if (!data) {
-	    if (mc_weight == 0) {
-	      event_weight = 0;
-	    }
-	    else {
-	      event_weight = mc_weight/fabs(mc_weight)*GetReweight_mumu(mc_trueNumInteractions, mu_gt_pt->at(iMu), mu_gt_eta->at(iMu), mu_gt_pt->at(iMu2), mu_gt_eta->at(iMu2));
-	    }
-	    }*/
 	  if (tauormu) break;
 	}//second muon loop
 	if (tightmuon && tauormu) break;
@@ -161,24 +122,10 @@ void IIHEAnalysis::Loop(string phase, string type_of_data, string out_name, stri
 	for (unsigned int iTau = 0; iTau < tau_pt->size(); ++iTau) {
 	  if (tau_pt->at(iTau) < 17.0) continue;
 	  if (fabs(tau_eta->at(iTau)) > 2.3) continue;
-	  //if (tau_decayModeFinding->at(iTau) < 0.5) continue;
 	  if (tau_againstMuonTight3->at(iTau) < 0.5) continue;
 	  if (tau_againstElectronVLooseMVA6->at(iTau) < 0.5) continue;
-	  //if (tau_ptLeadChargedCand->at(iTau) < 5) continue;
-	  if (Nmu > 1) continue;
 
 	  tauormu = true;
-
-	  /*if (!data) {
-	    if (mc_weight == 0) {
-	      event_weight = 0;
-	    }
-	    else {
-	      event_weight = mc_weight/fabs(mc_weight)*GetPUTriggerMuonIDMuonIsoReweight(mc_trueNumInteractions, mu_gt_pt->at(iMu), mu_gt_eta->at(iMu));
-	    }
-	  }*/
-
-
 	  if (tauormu) break;
 	}//loop over taus
 	if (tightmuon && tauormu) break;
--- script_jobs/MainScripts/make_submit_script_skims2017.py
+++ script_jobs/MainScripts/make_submit_script_skims2017.py
@@ -5,7 +5,7 @@
 #Note : your code needs to be already compiler (with a .exe extension for this to work)
 
 import os
-from datasets2017_new import * #imports dataset paths: pnfn[], myname[] and myoption[] arrays 
+from datasets2017 import * #imports dataset paths: pnfn[], myname[] and myoption[] arrays 
 if __name__ == "__main__":
     location=os.getcwd();
     #name of your *compiled* code (omit the .exe extension)
@@ -20,17 +20,17 @@ if __name__ == "__main__":
         name_out =  myname[jj] + ".sh"
         outFile = open("../Jobs_to_submit/"+name_out , 'w')
         command1 = "source $VO_CMS_SW_DIR/cmsset_default.sh " + "\n"
-        command1 = command1 + "cd /user/dbeghin/CMSSW_9_2_6/src " + "\n" #go to the top of your CMSSW release
+        command1 = command1 + "cd /user/dbeghin/2nd/CMSSW_9_4_0/src " + "\n" #go to the top of your CMSSW release
         command1 = command1 + "eval `scram runtime -sh` " + "\n"   #load the cms environment
         command1 = command1 + "export X509_USER_PROXY=/user/dbeghin/x509up_u$(id -u dbeghin)" + "\n"
-        command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/withTauMVA/Out_"+myname[jj] + "\n"
+        command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/2018May17/New/Out_"+myname[jj] + "\n"
         command1 = command1 + "export scratchdir=$TMPDIR " + "\n"
         command1 = command1 + "cd $scratchdir" + "\n"      
         command1 = command1 + "mkdir Reweighting" + "\n"
-        command1 = command1 + "cp /user/dbeghin/Work/Taus/Reweighting/*.root Reweighting/" + "\n"
-        command1 = command1 + "cp /user/dbeghin/Work/Taus/" + code_name + ".exe $scratchdir/" + "\n"
-        #command1 = command1 + "export workdir=/user/dbeghin/Work/Taus/ " + "\n"  #Set your working directory, where the code is located
-        #command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/withTauMVA/Out_"+myname[jj] + "\n"      #Make output directories
+        command1 = command1 + "cp /user/dbeghin/Work/POGTau/Reweighting/*.root Reweighting/" + "\n"
+        command1 = command1 + "cp /user/dbeghin/Work/POGTau/" + code_name + ".exe $scratchdir/" + "\n"
+        #command1 = command1 + "export workdir=/user/dbeghin/Work/POGTau/ " + "\n"  #Set your working directory, where the code is located
+        #command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/2018May17/New/Out_"+myname[jj] + "\n"      #Make output directories
         #command1 = command1 + "cd $workdir"
         outFile.write(command1)
         command3 = "qsub -q localgrid@cream02 -o " + "../out_err/"+myname[jj] +".stdout -e " +"../out_err/"+myname[jj] +".stderr -l walltime=80:00:00    " + "../Jobs_to_submit/"+name_out + "\n"  #Command to submit one job to the localgrid
@@ -45,26 +45,26 @@ if __name__ == "__main__":
                 outFile.close()
                 outFile = open("../Jobs_to_submit/test"+str(ligne/files_per_job)+name_out , 'w')
                 command1 = "source $VO_CMS_SW_DIR/cmsset_default.sh " + "\n"
-                command1 = command1 + "cd /user/dbeghin/CMSSW_9_2_6/src " + "\n"
+                command1 = command1 + "cd /user/dbeghin/2nd/CMSSW_9_4_0/src " + "\n"
                 command1 = command1 + "eval `scram runtime -sh` " + "\n"
                 command1 = command1 + "export X509_USER_PROXY=/user/dbeghin/x509up_u$(id -u dbeghin)" + "\n"
                 command1 = command1 + "export scratchdir=$TMPDIR " + "\n"
-                #command1 = command1 + "export workdir=/user/dbeghin/Work/Taus/ " + "\n"
+                #command1 = command1 + "export workdir=/user/dbeghin/Work/POGTau/ " + "\n"
                 command1 = command1 + "cd $scratchdir" + "\n"      
                 command1 = command1 + "mkdir Reweighting" + "\n"
-                command1 = command1 + "cp /user/dbeghin/Work/Taus/Reweighting/*.root Reweighting/" + "\n"
-                command1 = command1 + "cp /user/dbeghin/Work/Taus/" + code_name + ".exe $scratchdir/" + "\n"
-                #command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/withTauMVA/Out_"+myname[jj] + "\n"
+                command1 = command1 + "cp /user/dbeghin/Work/POGTau/Reweighting/*.root Reweighting/" + "\n"
+                command1 = command1 + "cp /user/dbeghin/Work/POGTau/" + code_name + ".exe $scratchdir/" + "\n"
+                #command1 = command1 + "gfal-mkdir srm://maite.iihe.ac.be:/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/2018May17/New/Out_"+myname[jj] + "\n"
                 outFile.write(command1)
                 command3 = "qsub -q localgrid@cream02 -o "+ "../out_err/test"+str(ligne/files_per_job)+myname[jj] +".stdout -e " +"../out_err/test"+str(ligne/files_per_job)+myname[jj] +".stderr -l walltime=80:00:00    " + "../Jobs_to_submit/test"+str(ligne/files_per_job)+name_out + "\n"
                 submit_File.write(command3)
             #Below, the command to execute your code, use the correct syntax for your own code, with all arguments (file in, file out, etc.) in the proper order
             #Note that i[0:-1] is just the name of the root file in /pnfs
             command2 = "dccp dcap://maite.iihe.ac.be"+   i[0:-1] + " $scratchdir/" + "\n"
-            #command2 = command2 + "cp /user/dbeghin/Work/Taus/" + code_name + ".exe $scratchdir/" + "\n"
+            #command2 = command2 + "cp /user/dbeghin/Work/POGTau/" + code_name + ".exe $scratchdir/" + "\n"
             bare_fname = i[len(pnfn[jj]):-1]
             command2 = command2 + "\n" + "./" + code_name + ".exe " + "Out" +str(ligne)+myname[jj]+ ".root " +   bare_fname + " " + mynick[jj] + " stuff " + myoption[jj]
-            command2 = command2 + " \n"+ " gfal-copy -f  Out" +str(ligne)+myname[jj]+ ".root \t" + "srm://maite.iihe.ac.be:8443/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/withTauMVA/Out_"+myname[jj]+"/Out" +str(ligne)+myname[jj]+ ".root"
+            command2 = command2 + " \n"+ " gfal-copy -f  Out" +str(ligne)+myname[jj]+ ".root \t" + "srm://maite.iihe.ac.be:/pnfs/iihe/cms/store/user/dbeghin/Skims/2017/2018May17/New/Out_"+myname[jj]+"/Out" +str(ligne)+myname[jj]+ ".root"
             command2 = command2 + "\n rm Out" +str(ligne)+myname[jj]+ ".root"
             command2 = command2 + " \n\n\n"
             outFile.write(command2)
